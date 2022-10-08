7d768c6dd594a7232cea4fff19971213b31b8c86
shovonkhan200h <shovonkhan00@gmail.com>
2022-10-09 04:54:38 +0600

    added if else grade system

---
+++ ifElseCheckGrade.js
@@ -0,0 +1,31 @@
+let numer= 39;
+
+if(numer >=80 && numer <=100){
+    console.log(`A+`)
+}
+
+else if(numer >=70 && numer <=79){
+    console.log(`A-`)
+}
+
+else if(numer >=60 && numer <=69){
+    console.log(`B`)
+}
+
+else if(numer >=50 && numer <=59){
+    console.log(`C`)
+}
+
+else if(numer >=40 && numer <=40){
+    console.log(`Pass`)
+}
+else{
+    console.log(`Failed`)
+}
+
+
+
+
+
+
+

6f317993cc66316d5efd907ea961883e7c30ac75
shovonkhan200h <shovonkhan00@gmail.com>
2022-10-09 03:12:53 +0600

    parse float and intiger updated

--- javascript/parseintfloat.js
+++ javascript/parseintfloat.js
@@ -1,1 +1,11 @@
-console.log(`hello`)
+let number1 = 10;
+let number2 = '2.5'; 
+number2= parseFloat(number2); // convert string to number
+number= +number2; // convert string to number 2nd method
+console.log(number1 + number2);
+
+let number3= 10;
+let number4= '2.5';
+number3= parseInt(number3); // convert number to string
+number3= ''+number3; // convert string to number 2th method
+console.log(number3 + number4);

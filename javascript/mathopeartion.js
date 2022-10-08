6e82612a27f584a9819f95e416c9317c919b2031
shovonkhan200h <shovonkhan00@gmail.com>
2022-10-09 03:42:55 +0600

    added mathoperation

---
+++ javascript/mathopeartion.js
@@ -0,0 +1,16 @@
+var number = -5;
+number=  Math.abs(number); // convert nagetive number to positive number
+console.log(number);
+
+var number2= 20.3;
+number2= Math.round(number2); // if 20.4 than it will 20 else 20.5 it will be 21 
+console.log(number2);
+
+
+var number3= 20.3;
+number3= Math.ceil(number3);
+console.log(number3) // whatever the value it will show uper value will be
+
+var number4= 20.3;
+number4= Math.floor(number4);
+console.log(number4) // whatever the value it will show lower value will be

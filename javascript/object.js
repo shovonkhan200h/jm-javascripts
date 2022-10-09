87d112391286a9958539d47626e95c6c8870312d
shovonkhan200h <shovonkhan00@gmail.com>
2022-10-10 03:11:00 +0600

    upload Object Update value, Add New value, 3 three method of acces object

---
+++ javascript/object.js
@@ -0,0 +1,15 @@
+let data = {id:121, phone:01666, name: 'John'}
+let data2 = {id:122, phone:06621221, name: 'John'}
+
+
+data2.bike=`yamaha` //Add new value in object
+data.name= `shovon` //update value in object
+let nameName= data2.name //first method to access in object
+let idd= data2['phone'] //second method to access in object
+let phoneNo= `phone` // third method to access in object
+
+
+
+
+
+

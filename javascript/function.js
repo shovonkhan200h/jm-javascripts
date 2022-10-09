c0205f50f5bb711d9b324e8e93c6da72a65567c6
shovonkhan200h <shovonkhan00@gmail.com>
2022-10-10 02:51:58 +0600

    upload function with add,minus,divided,multi

---
+++ javascript/function.js
@@ -0,0 +1,28 @@
+function add(num1,num2){
+    let result= num1+num2;
+    return result;
+}
+
+function minus(num1,num2){
+    let result= num1-num2;
+    return result;
+}
+
+
+function divied(num1,num2){
+    let result= num1/num2;
+    return result;
+}
+
+function multi(num1,num2){
+    let result= num1*num2;
+    return result;
+}
+
+
+console.log(add(10,10))
+console.log(minus(10,10))
+console.log(divied(10,10))
+console.log(multi(10,10))
+
+//Using value as peremter to set the value 

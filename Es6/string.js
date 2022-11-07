87b2912a34b0db34139f618034bfc2e8e1433f5d
shovonkhan200h <shovonkhan00@gmail.com>
2022-11-08 02:39:11 +0600

    string

---
+++ Es6/string.js
@@ -0,0 +1,12 @@
+const firstName =  `md mazharul`
+const lastName =`Islam`
+const total = firstName + ` ` +lastName;
+const total2= `${firstName} ${lastName} web developer`
+console.log(total2);
+
+
+const multiLine= "i love you\n" + "i miss you\n" + "i need you";
+const multiLine2 =`i love you
+i miss you
+i need you`
+console.log(multiLine2)

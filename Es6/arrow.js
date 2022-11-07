86b5ae89689721f3731b4eca941afbd47161b086
shovonkhan200h <shovonkhan00@gmail.com>
2022-11-08 03:01:33 +0600

    arrow function

---
+++ Es6/arrow.js
@@ -0,0 +1,13 @@
+function myFunction(num){
+    return num*2;
+}
+
+const myFunction2 = function myFunction(num){
+    return num * 2;
+}
+
+const myFunction3 = (num)=>{
+    return num * 2;
+}
+
+const myFunction4 = num => num * 2;

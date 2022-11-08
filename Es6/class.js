05613de62a7c3f61b67274099b6ea37e15a61a81
shovonkhan200h <shovonkhan00@gmail.com>
2022-11-08 14:08:43 +0600

    class

---
+++ Es6/class.js
@@ -0,0 +1,9 @@
+class Student {
+    constructor(studentId, studentName){
+        this.id = studentId;
+        this.name = studentName;
+    }
+}
+
+const student1= new Student(1, `shovon`)
+console.log(student1)

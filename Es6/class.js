e8ea8c645964009f315882ed917b1133cbad0a2e
Md Mazharul Islam <59208564+shovonkhan200h@users.noreply.github.com>
2023-08-02 22:45:05 +0100

    Update class.js

--- Es6/class.js
+++ Es6/class.js
@@ -7,3 +7,23 @@ class Student {
 
 const student1= new Student(1, `shovon`)
 console.log(student1)
+
+class Parent {
+  constructor(fatherName) {
+    this.fatherName = fatherName;
+  }
+}
+
+class Child extends Parent {
+  constructor(fatherName, name) {
+    super(fatherName);
+    this.name = name;
+  }
+
+  fullName() {
+    return this.fatherName + ' ' + this.name;
+  }
+}
+
+const baby = new Child('Adnan', 'Tuba');
+console.log(baby.fullName());
